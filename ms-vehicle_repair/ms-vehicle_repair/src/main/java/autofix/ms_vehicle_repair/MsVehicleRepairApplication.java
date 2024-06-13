e98823ff231a34c90560dc59e3e04c89806cf41c
Brayito <bryan.sepulveda@usach.cl>
2024-06-13 16:55:12 -0400

    Inicializacion

---
+++ ms-vehicle_repair/ms-vehicle_repair/src/main/java/autofix/ms_vehicle_repair/MsVehicleRepairApplication.java
@@ -0,0 +1,13 @@
+package autofix.ms_vehicle_repair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVehicleRepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVehicleRepairApplication.class, args);
+	}
+
+}
---
+++ ms-vehicle_repair/ms-vehicle_repair/src/test/java/autofix/ms_vehicle_repair/MsVehicleRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package autofix.ms_vehicle_repair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVehicleRepairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
